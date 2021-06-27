75b23256f853e149e9cb499876c364ad9bd3aac5
kinuvirani <kiran.virani1997@gmail.com>
2021-06-27 15:37:35 +0530

    changes in content

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -119,14 +119,12 @@ const Contact = ({ data }) => {
 
         <aside className="four columns footer-widgets">
           <div className="widget widget_contact">
-            <h4>Address and Phone</h4>
+            <h4>Contact Details</h4>
             <p className="address">
               {contactName}
               <br />
               {contactEmail}
               <br />
-              <br />
-              {street} <br />
               {city}, {state} {zip}
               <br />
               <span>{phone}</span>
